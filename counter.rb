1f6fe0ddaf6eeb15ab11127bc467bfd7565b26b3
tnx6devine <tnx6devine@gmail.com>
2017-11-19 19:54:19 -0500

    passed counting by 5s

--- counter.rb
+++ counter.rb
@@ -7,3 +7,19 @@ end
 def counterClass(test_number)
 	test_number.class
 end
+
+def counting(test_number)
+	counter = test_number
+	10.times do
+		$counterAr.push(counter)
+		counter += test_number
+	end
+	puts $counterAr
+	if $counterAr.last % 5 == 0
+		true
+	else
+		false
+	end
+
+
+end
